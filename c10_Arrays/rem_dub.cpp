a147bc856d6f17d6145925f08dacedd2bd64666a
Pratham <pratham10600@gmail.com>
2025-10-29 22:48:15 +0530

    rem_dublicate

---
+++ c10_Arrays/rem_dub.cpp
@@ -0,0 +1,37 @@
+// just insert the unique element in the same array and after rest elemnts same in array
+#include<bits/stdc++.h>
+using namespace std;
+
+// another method bad approach store in set then in array place
+
+bool remove_dub(int arr[],int n){
+
+   int j=0;
+   for(int i=0;i<n;i++){
+    if(arr[j]!=arr[i]){
+        j++;
+        arr[j]=arr[i];
+    }
+    }
+
+cout<<"removed"<<endl;
+for(int i=0;i<n;i++){
+    cout<<arr[i]<<endl;
+}
+
+cout<<"unique elemets are "<<j+1<<endl;
+
+}
+
+int main(){
+    int n;
+    cout<<"Enter number of element ";
+    cin>>n;
+    int arr[n];
+    for(int j=0;j<n;j++){
+        cin>>arr[j];
+    }
+
+    remove_dub(arr,n);
+
+}
