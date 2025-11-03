53d73a3aa949e8950b39d94cbb520e25a432ba73
Pratham <pratham10600@gmail.com>
2025-11-03 21:36:18 +0530

    optimal

--- c10_Arrays/move_zero.cpp
+++ c10_Arrays/move_zero.cpp
@@ -3,6 +3,62 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// bruteforce
+
+int move(vector<int> arr, int n){
+
+    // storing non zero elements
+    vector<int> temp;
+    for(int i=0;i<n;i++){
+        if (arr[i]!=0){
+            temp.push_back(arr[i]);
+        }
+    }
+
+    // push non zero to original array
+    int n2=temp.size();
+    for(int j=0;j<n2;j++){
+        arr[j]=temp[j];
+    }
+
+    // add zeros on last 
+    for(int j=n2;j<n;j++){
+        arr[j]=0;
+    }
+    
+    // printing resultant
+    for(int j=0;j<n;j++){
+        cout<<arr[j];
+    }
+
+    // tc=O(n) SC= O(n)
+}
+
+// Optimal 2 pointer approach
+
+int move_back(vector<int> arr, int n){
+    int j=-1;
+    for(int i=0;i<n;i++){
+        if (arr[i]==0){
+            j=i;
+            break;
+        }
+    }
+
+    for(int i=j+1;i<n;i++){
+        if(arr[i]!=0){
+            swap(arr[i],arr[j]);
+            j++;
+        }
+    }
+
+    // printing resultant
+    for(int j=0;j<n;j++){
+        cout<<arr[j];
+    }
+}
+
+// mine -> like bubble swap
 int replaced(int arr[],int n){
 
     for(int j=0;j<n-1;j++){
@@ -23,10 +79,13 @@ int main(){
     cout<<"enter number of elements";
     cin>>n;
 
-    int arr[n];
+    
+    vector<int> arr;
     for(int j=0;j<n;j++){
-        cin>>arr[j];
+        int temp;
+        cin>>temp;
+        arr.push_back(temp);
     }
 
-    replaced(arr,n);
+    move_back(arr,n);
 }
--- c10_Arrays/union.cpp
+++ c10_Arrays/union.cpp
@@ -3,6 +3,74 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+// brute force
+vector<int> sorted_union_Array(vector <int> a, vector<int> b){
+    int n1 = a.size();
+    int n2= b.size();
+    set<int> st;
+    for(int i=0;i<n1;i++){
+        st.insert(a[i]);
+    }
+    for(int j=0;j<n2;j++){
+        st.insert(b[j]);
+    }
+
+    vector<int> temp;
+    for(auto it:st){
+        temp.push_back(it);
+    }
+    return temp;
+
+}
+
+// Optimal -> two pointer approch
+// tc= O(n1+n2) sc=O(n1+n2)
+
+vector<int> sorted_union_Array2(vector <int> a, vector<int> b){
+    vector<int> union_arr;
+
+    int n1=a.size();
+    int n2=b.size();
+
+    int i=0;
+    int j=0;
+
+    while(i<n1 && j<n2){
+        if (a[i]<=b[j]){
+            if(union_arr.size()==0 || union_arr.back()!=a[i]){
+                union_arr.push_back(a[i]);
+            }
+            i++;
+        }else{
+            if(union_arr.size()==0 || union_arr.back()!=b[j]){
+                union_arr.push_back(b[j]);
+            }
+            j++;
+        }
+    }
+
+    while (i<n1){
+        if (a[i]<=b[j]){
+            if(union_arr.size()==0 || union_arr.back()!=a[i]){
+                union_arr.push_back(a[i]);
+            }
+            i++;
+        }
+    }
+
+    while (j<n2)
+    {
+        if(union_arr.size()==0 || union_arr.back()!=b[j]){
+                union_arr.push_back(b[j]);
+            }
+            j++;
+    }
+    
+    return union_arr;
+}
+
+// mine -> uniqueness still lacks
 int union_arrays(int arr1[],int n1,int arr2[],int n2){
     int arr3[n1+n2];
     for(int i=0;i<n1;i++){
@@ -21,24 +89,53 @@ cout<<"After merging"<<endl;
 }
 
 int main(){
+
     int n1;
     cout<<"enter number of elements in array 1 ";
     cin>>n1;
 
-    int arr1[n1];
-    for(int j=0;j<n1;j++){
-        cin>>arr1[j];
+    vector<int> a;
+    for(int i=0;i<n1;i++){
+        int temp;
+        cin>>temp;
+        a.push_back(temp);
     }
 
     int n2;
-    cout<<"enter number of elements in array 2 ";
+    cout<<"enter number of elements in array 1 ";
     cin>>n2;
-    
-    int arr2[n2];
-    for(int j=0;j<n2;j++){
-        cin>>arr2[j];
+
+    vector<int> a2;
+    for(int i=0;i<n2;i++){
+        int temp;
+        cin>>temp;
+        a2.push_back(temp);
     }
-    
-union_arrays(arr1,n1,arr2,n2);
-    
+    cout<<"After union"<<endl;
+    vector<int> result=sorted_union_Array2(a,a2);
+    for(auto it:result){
+        cout<<it<<endl;
+    }
+
 }
+//     int n1;
+//     cout<<"enter number of elements in array 1 ";
+//     cin>>n1;
+
+//     int arr1[n1];
+//     for(int j=0;j<n1;j++){
+//         cin>>arr1[j];
+//     }
+
+//     int n2;
+//     cout<<"enter number of elements in array 2 ";
+//     cin>>n2;
+    
+//     int arr2[n2];
+//     for(int j=0;j<n2;j++){
+//         cin>>arr2[j];
+//     }
+    
+// union_arrays(arr1,n1,arr2,n2);
+    
+// }
