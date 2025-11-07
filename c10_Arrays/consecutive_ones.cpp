8751a8952981b18907a57977d77a9fd34059e39d
Pratham <pratham10600@gmail.com>
2025-11-07 21:57:48 +0530

    optimal

--- c10_Arrays/consecutive_ones.cpp
+++ c10_Arrays/consecutive_ones.cpp
@@ -2,7 +2,32 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-
+int max_iter(vector<int> a,int n){
+    int maxi=0;
+    int count=0;
+    for(int i=0;i<n;i++){
+        if(a[i]==1){
+            count+=1;
+            maxi=max(count,maxi);
+        }
+        else{
+            count=0;
+        }
+    }
+    return maxi;
+}
 int main(){
+    int n;
+    cout<<"Enter no of Elements: ";
+    cin>>n;
+    vector<int> arr;
+    for(int i=0;i<n;i++){
+        int temp;
+        cin>>temp;
+        arr.push_back(temp);
+    }
+    cout<<"Max iterated 1: "<<endl;
+    cout<<max_iter(arr,n);
+    
     
 }
--- c10_Arrays/single.cpp
+++ c10_Arrays/single.cpp
@@ -19,6 +19,49 @@ Explanation: In this array, only element 4 appear once and the other elements ap
 #include<bits/stdc++.h>
 using namespace std;
 
+// brute force
+// tc=n^2
+int single2(int arr[],int n){
+    for (int i=0;i<n;i++){
+        int count =0;
+        for(int j=0;j<n;j++){
+            if (arr[i]==arr[j]){
+                count++;
+            }
+        }
+        if (count==1){
+            return arr[i];
+        }
+    }
+}
+
+// Better
+/*
+1. using hash ie one arr of size max of elemnet. iterate thoruogh arr then +1 the index of new arr.
+if count ==1 return that element
+
+2. or
+use map iterate thoough map if map.second==1 return map.first 
+tc= nlogn for sorted map and n for unordered only n2 in worst case
+sc= O(n/2)
+*/
+
+// Optimal
+
+/*
+XOR-> 0^0=0 but 0^2=2
+means return 0 if same no xor and no itself if non same
+tc=O(n)
+*/
+int single3(int arr[], int n){
+    int xorr=0;
+    for (int i=0;i<n;i++){
+        xorr=xorr ^ arr[i];
+    }
+    return xorr;
+}
+
+// mine
 int single(int arr[],int n){
 
     sort(arr,arr+n);
@@ -42,5 +85,5 @@ int main(){
         cin>>arr[j];
     }
 
-    cout<<"Non twice element is"<<single(arr,n);
+    cout<<"Non twice element is"<<single3(arr,n);
 }
