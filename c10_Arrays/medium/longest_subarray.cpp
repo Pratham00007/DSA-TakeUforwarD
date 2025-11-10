c5d9514be67a324f7c1047a2853c3592ee5b6bbf
Pratham <pratham10600@gmail.com>
2025-11-10 21:57:43 +0530

    optimized

--- c10_Arrays/medium/longest_subarray.cpp
+++ c10_Arrays/medium/longest_subarray.cpp
@@ -5,7 +5,8 @@ Given an array and a sum k, we need to print the length of the longest subarray 
 #include<bits/stdc++.h>
 using namespace std;
 
-int subarray(vector<int> arr,int n,int k){
+// Brute force
+int subarray(vector<int> arr,int n,int target){
     int sum;
     int j=0;
     for(int k=0;k<n;k++){
@@ -13,14 +14,50 @@ int subarray(vector<int> arr,int n,int k){
         sum=0;
         for(int j=k;j<i;j++){
             sum+=arr[j];
-            if (sum==k){
-                return j-k;
+            if (sum==target){
+                return j-k+1;
             }
         }
 
     }
 }
     return -99;
+}
+
+// better -> samjh nhi aaya
+
+// optimal
+/*
+two pointer approach 
+let se add karo 
+sum me add karte jao
+if sum>target 
+left index ka element hatao righ ka daaloo
+and still if sum=target tab bhi len me store karwa do lenght of subarray 
+but shift to right subtracting left if that lenght > prevoious store that
+if sum> target subtract left again add right 
+*/
+
+int subarray2(vector<int> arr,int n,int target){
+    int sum = arr[0];
+    int maxlen=0;
+    int left=0;
+    int right =0;
+    while (right<n){
+        while(left<=right && sum>target){
+            sum-= arr[left];
+            left++;
+        }
+        if(sum==target){
+            maxlen=max(maxlen,right-left+1);
+
+        }
+        right++;
+        if(right<n) sum += arr[right];
+        
+
+    }
+    return maxlen;
 }
 
 
@@ -37,5 +74,5 @@ int subarray(vector<int> arr,int n,int k){
     int target;
     cout<<"Enter the target";
     cin>>target;
-    cout<<"max size of sub array "<<subarray(arr,n,target);
+    cout<<"max size of sub array "<<subarray2(arr,n,target);
  }
